0a10532b02d5c89931280c9371a3eb194f31d5a8
Dmitriev Oleg <odmitriev8051@gmail.com>
2020-03-19 13:25:41 +0300

    bug fix

--- unpack_str/main.go
+++ unpack_str/main.go
@@ -6,39 +6,35 @@ import (
 )
 
 func main() {
-	const s string = "gJd8SA0!3" // testing string
-	var l string = string(s[0])  //storage for the current letter
-	var n string = "_"           //storage for the current number
+	const s string = "sdas" // testing string
+	var l string   //storage for the current letter
+	var n string = "_"      /*storage for the current number.
+							 "_" means that the number of prints of the lettet is 1 */
 
 	for i := 0; i < len(s); i++ {
 
 		/* if this is the last element, then s[i+1] will break the program,
 		so another algorithm is used for this iteration*/
 		if i == len(s)-1 {
-			if !f.IsNumeral(string(s[i])) {
-				fmt.Printf(string(s[i]))
-			} else {
-				if f.IsNumeral(string(s[i])) {
-					n += string(s[i])
-					f.PrintLetter(f.StrToInt(n), l)
-				} else {
-					fmt.Printf("%v", string(s[i]))
-				}
-			}
-			continue
 
+			if !f.IsNumeral(string(s[i])) {
+				l = string(s[i])
+				f.PrintLetter(f.StrToInt(n), l)
+				n = "_"
+			} else {
+				n += string(s[i])
+				f.PrintLetter(f.StrToInt(n), l)
+			}
+
+			break
+		
 			/* for all iterations except the last */
 		} else {
+
 			if !f.IsNumeral(string(s[i])) {
 				l = string(s[i])
 			} else {
-				if f.IsNumeral(string(s[i])) {
-					n += string(s[i])
-				} else {
-					fmt.Printf("%v", string(s[i]))
-
-				}
-
+				n += string(s[i])
 			}
 		}
 
