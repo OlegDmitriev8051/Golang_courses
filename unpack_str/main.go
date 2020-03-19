996815d12206f674a8dcdf1676ae9da826c27a7f
Dmitriev Oleg <odmitriev8051@gmail.com>
2020-03-19 13:29:12 +0300

    bug fix

--- unpack_str/main.go
+++ unpack_str/main.go
@@ -2,14 +2,15 @@ package main
 
 import (
 	"fmt"
+
 	f "github.com/OTUS/unpack_str/functions"
 )
 
 func main() {
 	const s string = "sdas" // testing string
-	var l string   //storage for the current letter
+	var l string            //storage for the current letter
 	var n string = "_"      /*storage for the current number.
-							 "_" means that the number of prints of the lettet is 1 */
+	"_" means that the number of prints of the lettet is 1 */
 
 	for i := 0; i < len(s); i++ {
 
@@ -20,14 +21,14 @@ func main() {
 			if !f.IsNumeral(string(s[i])) {
 				l = string(s[i])
 				f.PrintLetter(f.StrToInt(n), l)
-				n = "_"
+				n = "_" // n = 1
 			} else {
 				n += string(s[i])
 				f.PrintLetter(f.StrToInt(n), l)
 			}
 
 			break
-		
+
 			/* for all iterations except the last */
 		} else {
 
@@ -42,7 +43,7 @@ func main() {
 		   then print the previous letter */
 		if !f.IsNumeral(string(s[i+1])) {
 			f.PrintLetter(f.StrToInt(n), l)
-			n = "_"
+			n = "_" // n = 1
 		}
 
 	}
